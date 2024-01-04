72db55988e987f7c1a38c7fb0e8e798fbac56476
Edgar Gonzalez <gonzalezedgarphone@gmail.com>
2024-01-03 20:33:37 -0800

    first commit - webscrape title extract for travel

---
+++ webscrape.py
@@ -0,0 +1,17 @@
+
+from bs4 import BeautifulSoup
+import requests
+
+url = "http://books.toscrape.com/catalogue/category/books/travel_2/index.html"
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+image_content = soup.find_all('div', class_='image_container')
+
+for content in image_content:
+    img_tag = content.find('img')
+
+    title = img_tag['alt']
+
+    print(f'Title: {title}')
